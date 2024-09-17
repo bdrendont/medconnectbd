019905aaae28b7b4778cf1e56639f25ea849c4df
bdrendont <162622383+bdrendont@users.noreply.github.com>
2024-09-17 17:53:49 -0500

    Update configdb.php
    Agregando datos de usuario

--- controller/configdb.php
+++ controller/configdb.php
@@ -2,9 +2,9 @@
 class ConfigDb {
     private $db_host = "localhost"; //Lugar o IP donde esta el servidor de Base de datos
     private $db_port = "3306"; // Puerto de Base de datos MySQL
-    private $db_user = "root"; // Nombre del usuario para conectarnos a la base de datos 
-     private $db_pass = ""; // Contraseña del Usuario de la Base de datos
-    private $db_name = "proovinos"; // Nombre de la Base de Datos
+    private $db_user = "brignidb"; // Nombre del usuario para conectarnos a la base de datos 
+     private $db_pass = "Brignir123"; // Contraseña del Usuario de la Base de datos
+    private $db_name = "medconnect"; // Nombre de la Base de Datos
 
     public function conexion(){
         $link = "mysql:host=".$this->db_host.":".$this->db_port.";dbname=".$this->db_name.";";
